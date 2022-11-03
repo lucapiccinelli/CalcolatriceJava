d68f40644728a9c9d8436bc9a9f183af289bc252
Michele Gressani <michele.gressani@gmail.com>
2022-11-03 10:22:04 +0100

    refactoring clear button

--- src/main/java/org/example/calcolatrice/Main.java
+++ src/main/java/org/example/calcolatrice/Main.java
@@ -19,49 +19,42 @@ public class Main
         panel.add(inputField);
 
 
-        createButton(20, 225, "1", panel);
+        buttonList(panel);
+        createClearButton(20, 295, panel);
 
-        createButton(110, 225, "2", panel);
-
-        createButton(200, 225, "3", panel);
-
-        createButton(20, 155,"4", panel);
-
-        createButton(110, 155,"5", panel);
-
-        createButton(200, 155, "6", panel);
-
-        createButton(20, 85, "7", panel);
-
-        createButton(110, 85, "8", panel);
-
-        createButton(200, 85, "9", panel);
-
-        createButton(110, 295, "0", panel);
-
-        createButton(20, 295, "C", panel);
-
-        createButton(200, 295, ",", panel);
-
-        createButton(290, 295, "+", panel);
-
-        createButton(290, 225, "-", panel);
-
-        createButton(290, 155, "X", panel);
-
-        createButton(290, 85, "÷", panel);
-
-        createButton(290, 15, "=", panel);
 
         frame.setSize(400, 400);
         frame.add(panel);
         frame.setVisible(true);
     }
 
+
+
+    private static void buttonList(JPanel panel)
+    {
+        createButton(20, 225, "1", panel);
+        createButton(110, 225, "2", panel);
+        createButton(200, 225, "3", panel);
+        createButton(20, 155,"4", panel);
+        createButton(110, 155,"5", panel);
+        createButton(200, 155, "6", panel);
+        createButton(20, 85, "7", panel);
+        createButton(110, 85, "8", panel);
+        createButton(200, 85, "9", panel);
+        createButton(110, 295, "0", panel);
+        createButton(200, 295, ",", panel);
+        createButton(290, 295, "+", panel);
+        createButton(290, 225, "-", panel);
+        createButton(290, 155, "X", panel);
+        createButton(290, 85, "÷", panel);
+        createButton(290, 15, "=", panel);
+    }
+
+
+
     private static void createButton(int x, int y, String text, JPanel panel)
     {
-        JButton button = new JButton(text);
-        button.setBounds(x, y, 70, 50);
+        JButton button = JButton(text, x, y);
         button.addActionListener(new ActionListener()
         {
             @Override
@@ -70,19 +63,43 @@ public class Main
                 JTextField inputField = (JTextField) panel.getComponent(0);
                 String inputFieldText = inputField.getText();
                 inputField.setText(inputFieldText + text);
-                cancelField(inputField, text);
             }
         });
         panel.add(button);
     }
 
-    private static void cancelField(JTextField inputField, String text)
+
+
+
+    private static void createClearButton(int x, int y, JPanel panel)
     {
-        if (text.contains("C"))
+        JButton button = JButton("C", x, y);
+        button.addActionListener(new ActionListener()
         {
-            inputField.setText("");
-        }
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                JTextField inputField = (JTextField) panel.getComponent(0);
+                clearField(inputField);
+            }
+        });
+        panel.add(button);
     }
+
+    private static JButton JButton(String C, int x, int y)
+    {
+        JButton button = new JButton(C);
+        button.setBounds(x, y, 70, 50);
+        return button;
+    }
+
+
+    private static void clearField(JTextField inputField)
+    {
+        inputField.setText("");
+    }
+
+
 
 
     private static JPanel createPanel(int width, int height)
